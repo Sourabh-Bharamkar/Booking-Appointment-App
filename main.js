a918a7c429b3334eb4e844c37e3442514171c01f
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-01-18 19:32:21 +0530

    added functionality of edit Appointments

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ let form = document.getElementById('form')
 
 let registeredUsers = document.getElementById('registeredUsers')
 
+
 //Sending data to CrudCrud on submit
 form.addEventListener('submit', storeDetails)
-
 
 //store user details on CrudCrud  
 function storeDetails(e) {
@@ -25,22 +25,66 @@ function storeDetails(e) {
     'phoneNumber': phoneNumber
   }
 
-
-  axios.post('https://crudcrud.com/api/17b7b3a9597e4d6e9fcff3e6d727b822/appointmentData', userDetails)
+  //check whether user is present with this email id 
+  //if present update the corresponding user details
+  axios.get('https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData')
     .then((response) => {
-      showUserOnScreen();
-    }).catch((error) => {
-      alert("something went wrong")
+      let allUserDetails = response.data;
+      var elementFound = false;
+
+      allUserDetails.forEach((element) => {
+        //if element is found with corresponding user email, update this element
+        if (userDetails.email == element.email) {
+          elementFound = true;
+          let id = element._id;
+
+          axios.put(`https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData/${id}`, userDetails)
+            .then((response) => {
+              //modify user details from screen 
+              let userList = Array.from(document.getElementById('registeredUsers').childNodes)
+
+              userList.forEach((item) => {
+                if (item.id == element.email) {
+                  console.log(item.firstChild)
+                  item.firstChild.remove()
+                  item.prepend(`${userDetails.userName} - ${userDetails.email} - ${userDetails.phoneNumber}`)
+                }
+              })
+              //clear input fields
+              document.getElementById('name').value = ''
+              document.getElementById('email').value = ""
+              document.getElementById('phoneNumber').value = ""
+
+
+            }).catch((error) => {
+              alert("something went wrong")
+            })
+        }
+
+      })
+
+      console.log(elementFound);
+
+      //if element is not found
+      if (elementFound == false) {
+        axios.post('https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData', userDetails)
+          .then((response) => {
+            showUserOnScreen();
+          }).catch((error) => {
+            alert("something went wrong")
+          })
+      }
     })
 
 }
+
 
 //add event listener to DOMContentLoaded
 window.addEventListener('DOMContentLoaded', getDataFromCrudCrud)
 
 function getDataFromCrudCrud(e) {
   e.preventDefault();
-  axios.get('https://crudcrud.com/api/17b7b3a9597e4d6e9fcff3e6d727b822/appointmentData')
+  axios.get('https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData')
     .then((response) => {
       response.data.forEach(element => {
         let userDetails = {
@@ -49,29 +93,16 @@ function getDataFromCrudCrud(e) {
           'phoneNumber': element.phoneNumber
         }
 
-        // create li element 
-        let li = document.createElement('li')
-        li.style.display = 'flex'
+        let userDetailsHTML = `<li id=${userDetails.email} display="flex"> ${userDetails.userName} - ${userDetails.email} - ${userDetails.phoneNumber} <input type='button' value="edit" class="btn btn-outline-success btn-sm mx-1 edit"> <input type='button' value="delete" class="btn btn-outline-danger btn-sm mx-1 delete"></li>`
 
-        // creating textnode for user details 
-        let userDetailsTextnode = document.createTextNode(`${userDetails.userName} - ${userDetails.email} - ${userDetails.phoneNumber} `);
-        li.appendChild(userDetailsTextnode);
 
-        //create edit and delete buttons
-        let editBtn = editButton();
-        let deleteBtn = deleteButton();
+        //append userDetailsHTML to registered Users 
+        document.getElementById("registeredUsers").insertAdjacentHTML("beforeend", userDetailsHTML)
 
-        //append these buttons to li
-        li.appendChild(editBtn)
-        li.appendChild(deleteBtn)
-
-        //append li to registered Users 
-        document.getElementById('registeredUsers').append(li)
-
-      });
+      })
 
     }).catch((error) => {
-      alert(error)
+      console.log(error)
     })
 
 }
@@ -86,28 +117,14 @@ function showUserOnScreen() {
   let userDetails = {
     "userName": userName,
     'email': email,
-    'PhoneNumber': phoneNumber
+    'phoneNumber': phoneNumber
   }
 
-  // create li element 
-  let li = document.createElement('li')
-  li.style.display = 'flex'
 
-  // creating textnode for user details 
-  let userDetailsTextnode = document.createTextNode(`${userDetails.userName} - ${userDetails.email} - ${userDetails.PhoneNumber}`)
-  
-  li.appendChild(userDetailsTextnode)
+  let userDetailsHTML = `<li id=${userDetails.email} display="flex"> ${userDetails.userName} - ${userDetails.email} - ${userDetails.phoneNumber} <input type='button' value="edit" class="btn btn-outline-success btn-sm mx-1 edit"> <input type='button' value="delete" class="btn btn-outline-danger btn-sm mx-1 delete"></li>`
 
-  //create edit and delete buttons
-  let editBtn = editButton();
-  let deleteBtn = deleteButton();
-
-  //append these buttons to li
-  li.appendChild(editBtn)
-  li.appendChild(deleteBtn)
-
-  //append li to registered Users 
-  document.getElementById('registeredUsers').append(li)
+  //append userDetailsHTML to registered Users 
+  document.getElementById("registeredUsers").insertAdjacentHTML("beforeend", userDetailsHTML)
 
   document.getElementById('name').value = ''
   document.getElementById('email').value = ""
@@ -115,28 +132,11 @@ function showUserOnScreen() {
 
 }
 
-function editButton() {
-  // create edit button 
-  let editBtn = document.createElement('input')
-  editBtn.setAttribute('type', 'button')
-  editBtn.setAttribute('value', 'Edit')
-  editBtn.className = 'btn btn-outline-success btn-sm mx-1 edit'
-  return editBtn;
-}
-
-function deleteButton() {
-  // create delete button 
-  let deleteBtn = document.createElement('input')
-  deleteBtn.setAttribute('type', 'button');
-  deleteBtn.setAttribute('value', 'Delete');
-  deleteBtn.className = 'btn btn-outline-danger btn-sm delete';
-  return deleteBtn;
-}
 
 //add delete functionality 
-registeredUsers.addEventListener('click', deleteFunction)
+registeredUsers.addEventListener('click', deleteUserDetails)
 
-function deleteFunction(e) {
+function deleteUserDetails(e) {
   e.preventDefault();
 
   if (e.target.classList.contains('delete')) {
@@ -160,21 +160,57 @@ function deleteFunction(e) {
 }
 
 function deleteUserFromCrudCrud(e) {
-  axios.get('https://crudcrud.com/api/17b7b3a9597e4d6e9fcff3e6d727b822/appointmentData')
+  axios.get('https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData')
     .then((response) => {
       let allUserDetails = response.data;
+
       allUserDetails.forEach((element) => {
-        //get target user details
-        let targetUserDetails = e.target.parentNode.firstChild.textContent;
-        
-        if (targetUserDetails.includes(element.email)) {
+        //get target user id
+        let targetUserId = e.target.parentNode.id;
+
+        if (targetUserId == element.email) {
 
           let id = element._id;
           console.log(id)
 
-          axios.delete(`https://crudcrud.com/api/17b7b3a9597e4d6e9fcff3e6d727b822/appointmentData/${id}`).then((response)=>{ console.log(`user with id ${id} is deleted`)}).catch(()=>{console.log(error)})
+          axios.delete(`https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData/${id}`).then((response) => { console.log(`user with id ${id} is deleted`) }).catch(() => { console.log(error) })
         }
       })
 
     })
 }
+
+
+registeredUsers.addEventListener('click', editUserDetails)
+
+// adding edit functionality 
+function editUserDetails(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('edit')) {
+    //find corresponding user details from crud crud 
+    axios.get('https://crudcrud.com/api/e1d8df644a6f486eb1b851a7b4be7ac2/appointmentData')
+      .then((response) => {
+
+        let allUserDetails = response.data;
+
+        allUserDetails.forEach((element) => {
+
+          //get target user id
+          let targetUserId = e.target.parentNode.id;
+
+          if (targetUserId == element.email) {
+            //put corresponding user details to input fields 
+            document.getElementById('name').value = element.userName;
+            document.getElementById('email').value = element.email;
+            document.getElementById('phoneNumber').value = element.phoneNumber;
+
+
+          }
+        })
+      })
+
+  }
+}
+
+
